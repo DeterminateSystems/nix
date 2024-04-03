cda15a65ee8fcd030cb8298cae904a46eee87136
Graham Christensen <graham@grahamc.com>
2024-04-03 13:08:46 -0400

    Add a helper to compare resulting nix.conf settings, and a failing test for sandboxes.

--- tests/flake.nix
+++ tests/flake.nix
@@ -13,45 +13,94 @@
     };
   };
 
-  outputs = { nixpkgs, nix, home-manager, nix-darwin, ... }: {
-    checks.x86_64-linux.nixos = (nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        nix.nixosModules.default
-        {
-          fileSystems."/" = {
-            device = "/dev/bogus";
-            fsType = "ext4";
-          };
-          boot.loader.grub.devices = [ "/dev/bogus" ];
-        }
-      ];
-    }).config.system.build.toplevel;
+  outputs = { nixpkgs, nix, home-manager, nix-darwin, ... }:
+    let
+      checkNixConfSetting = { system, binDir, configDir, build, expect }:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.runCommand "${build.name}-check"
+          {
+            buildInputs = [ pkgs.jq pkgs.dyff ];
+            expect = builtins.toJSON expect;
+            passAsFile = [ "expect" ];
+          } ''
+          set -o pipefail
 
-    checks.x86_64-linux.home-manager = (home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          NIX_CONF_DIR=${build}/${configDir} ${build}/${binDir}/nix config show --json \
+            | jq \
+              --sort-keys \
+              --slurpfile expect "$expectPath" \
+              '[to_entries[] | { key: .key, value: .value.value } | select(.key | in($expect[0]))] | from_entries' > rendered.json
+    
+          dyff between --set-exit-code ./rendered.json "$expectPath"
+          touch "$out"
+        '';
+    in
+    {
+      checks.x86_64-linux.nixos = (nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nix.nixosModules.default
+          {
+            fileSystems."/" = {
+              device = "/dev/bogus";
+              fsType = "ext4";
+            };
+            boot.loader.grub.devices = [ "/dev/bogus" ];
+          }
+        ];
+      }).config.system.build.toplevel;
 
-      modules = [
-        nix.homeManagerModules.default
-        {
-          home.stateVersion = "23.11";
-          home.username = "example";
-          home.homeDirectory = "/no-such/directory";
-          home.enableNixpkgsReleaseCheck = false;
-        }
-      ];
-    }).activation-script;
+      checks.x86_64-linux.home-manager = (home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-    checks.x86_64-darwin.nix-darwin = (nix-darwin.lib.darwinSystem {
-      system = "x86_64-darwin";
+        modules = [
+          nix.homeManagerModules.default
+          {
+            home.stateVersion = "23.11";
+            home.username = "example";
+            home.homeDirectory = "/no-such/directory";
+            home.enableNixpkgsReleaseCheck = false;
+          }
+        ];
+      }).activation-script;
 
-      modules = [
-        ({ pkgs, ... }: {
-          imports = [
-            nix.darwinModules.default
+      checks.x86_64-darwin.nix-darwin = (nix-darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+
+        modules = [
+          ({ pkgs, ... }: {
+            imports = [
+              nix.darwinModules.default
+            ];
+          })
+        ];
+      }).system;
+
+
+      checks.x86_64-darwin.nix-darwin-with-sandbox = checkNixConfSetting {
+        system = "x86_64-darwin";
+        binDir = "sw/bin";
+        configDir = "etc/nix";
+        expect = {
+          sandbox = true;
+          extra-sandbox-paths = [ "/private/etc/ssl/openssl.cnf" ];
+        };
+
+        build = (nix-darwin.lib.darwinSystem {
+          system = "x86_64-darwin";
+
+          modules = [
+            ({ pkgs, ... }: {
+              imports = [
+                nix.darwinModules.default
+              ];
+
+              nix.settings.sandbox = true;
+            })
           ];
-        })
-      ];
-    }).system;
-  };
+        }).system;
+      };
+    };
 }
