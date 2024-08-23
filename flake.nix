bf817bcf81bba0e721c96ff4ccfd43cf86cc25b7
Graham Christensen <graham@grahamc.com>
2024-08-23 10:18:25 -0400

    Update Nix to 2.24.4

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Determinate Nix";
   inputs = {
-    nix.url = "https://flakehub.com/f/NixOS/nix/=2.24.3";
+    nix.url = "https://flakehub.com/f/NixOS/nix/=2.24.4";
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
   };
 
